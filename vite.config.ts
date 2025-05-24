4d0f59c7a369b8eb784689ec44ba8ff42babdbd7
Ahmed Ezzat <bobobobobo722@gmail.com>
2025-05-25 00:21:03 +0300

    Build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dr-mohamed-abdelsaboor-portfolio1/', // 👈 لازم تكون نفس اسم الريبو على GitHub بالضبط
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
